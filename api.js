e2b5092d7e90356cd5b122e1a5fe57ed32d3d0d9
pjbronzal <preciousbronzal.bsit@gmail.com>
2023-09-30 18:09:57 +0800

    add to wishlist attempt - innerText not functional

--- api.js
+++ api.js
@@ -1,27 +1,33 @@
+let newCustomer = new Date().getTime();
+
 const customer = {
   orderlist: [],
   menulist: [
     {
       id: 1,
       productName: "Flower Bouquets",
+      category: "bouquets",
       price: 450,
       image: "img/1.jpg",
     },
     {
       id: 2,
       productName: "Flower",
+      category: "bouquets",
       price: 450,
       image: "img/1.jpg",
     },
     {
       id: 3,
       productName: "Flower Bouquets",
+      category: "bouquets",
       price: 450,
       image: "img/1.jpg",
     },
     {
       id: 4,
       productName: "Flower",
+      category: "bouquets",
       price: 450,
       image: "img/1.jpg",
     },
@@ -39,12 +45,12 @@ const customer = {
   
           <div class="card-body">
               <h6 class="card-title fw-bold text-center" id="menu${data.productName}">${data.productName}</h6>
-              <p class="card-text text-center" id="price${data.price}">P ${data.price}.00</p>
+              <p class="card-text text-center" id="price${data.price}">₱ ${data.price}.00</p>
               
               <!-- Add position-absolute to the container of the buttons -->
               <div class="container d-flex justify-content-center position-absolute bottom-0 start-50 translate-middle-x mb-2">
-                  <button class="btn btn-sm btn-success me-2"><i class="fa-regular fa-heart"></i></button>
-                  <button class="btn btn-sm btn-success"><i class="fa-solid fa-cart-shopping"></i></button>
+                  <button class="btn btn-sm btn-success me-2" onclick="addToWishlist(${data.id})"><i class="fa-regular fa-heart"></i></button>
+                  <button class="btn btn-sm btn-success" onclick="addToCart(${data.id})"><i class="fa-solid fa-cart-shopping"></i></button>
               </div>
           </div>
       </div>
@@ -55,5 +61,43 @@ const customer = {
   },
 };
 
+orderedlist = localStorage.getItem(newCustomer);
+
+function addToWishlist(id) {
+  let orderedlist = JSON.parse(localStorage.getItem(newCustomer)) || [];
+
+  const existingItemIndex = orderedlist.findIndex((item) => item.id === id);
+
+  if (existingItemIndex !== -1) {
+    alert("This item is already in your order."); //not working :<
+  } else {
+    let new_id = document.getElementById("ids" + id);
+    let new_order = document.getElementById("menu" + id);
+    let new_price = document.getElementById("price" + id);
+    // let new_price = parseFloat(
+    //   document
+    //     .getElementById("price" + id)
+    //     .innerText.replace("₱", "")
+    //     .replace(".00", "")
+    // );
+
+    orderedlist.push({
+      id: new_id,
+      productName: new_order,
+      price: new_price,
+      quantity: 1,
+      totalPrice: new_price,
+    });
+
+    localStorage.setItem(newCustomer, JSON.stringify(orderedlist));
+    // showorder();
+
+    alert("Added To Wishlist.");
+
+    // document.querySelector(`button[data-id="${id}"]`).disabled = true;
+  }
+  //   updateTotal();
+}
+
 customer.showproducts();
-showorder();
+// showorder();
