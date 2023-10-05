531a3889b5d8bb5cad977e69b62dd07eb8ca58fb
pjbronzal <preciousbronzal.bsit@gmail.com>
2023-10-05 19:08:53 +0800

    best-selling flower

--- wishlist/index.js
+++ wishlist/index.js
@@ -7,6 +7,7 @@ function displayWishlistItems() {
       return `
       <div class="col-3">
         <div class="card mb-3 position-relative">
+          <button class="remove pe-2"><i class="fa-solid fa-x"></i></button>
           <p id="ids${item.id}" hidden>${item.id}</p>
           <img id="image${item.id}" src="${item.image}" class="img-fluid pt-2 px-2">
     
@@ -14,7 +15,7 @@ function displayWishlistItems() {
               <p class="card-title fw-bold" id="menu${item.id}">${item.productName}</p>
               <p class="card-text" id="price${item.id}">₱ ${item.price}.00</p>
               <button class="btn btn-sm" onclick="addToCart(${item.id})"><i class="fa-solid fa-cart-shopping me-2"></i>Add To Cart</button>
-              <button class="btn btn-sm deleteWishItem" onclick=""><i class="fa-solid fa-trash"></i></button>
+              
           </div>
         </div>
       </div>
@@ -23,6 +24,30 @@ function displayWishlistItems() {
     .join("");
 
   wishlistDiv.innerHTML = wishlistHTML;
+}
+
+{
+  /* <button class="btn btn-sm deleteWishItem" onclick="deleteWishlistItem(${item.id})"><i class="fa-solid fa-trash"></i>Remove</button> */
+}
+
+function deleteWishlistItem(itemId) {
+  // Retrieve the current wishlist from local storage
+  const wishlistItems = JSON.parse(localStorage.getItem("wishlist")) || [];
+
+  // Find the index of the item with the given itemId in the wishlistItems array
+  const itemIndex = wishlistItems.findIndex((item) => item.id === itemId);
+
+  // Check if the item was found in the wishlist
+  if (itemIndex !== -1) {
+    // Remove the item from the wishlistItems array
+    wishlistItems.splice(itemIndex, 1);
+
+    // Update the localStorage with the modified wishlist
+    localStorage.setItem("wishlist", JSON.stringify(wishlistItems));
+
+    // Refresh the display of wishlist items
+    displayWishlistItems();
+  }
 }
 
 displayWishlistItems();
