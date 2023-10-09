da9b6f7b2c0c30b16de28137c4151803029ab8ed
Ann Marie Abella <63768184+annishie@users.noreply.github.com>
2023-10-09 05:23:07 -0700

    update login/register

--- js/main.js
+++ js/main.js
@@ -670,3 +670,20 @@
 renderProducts();
 
 
+//Login Functions / Register
+
+function login() {
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+
+  if (email == "" || password == "") {
+    alert("Please enter a valid email or password.");
+    location.reload();
+  } else {
+    let login_arr = [];
+    login_arr.push({ email: email });
+    sessionStorage.setItem("login", JSON.stringify(login_arr));
+    alert("You have successfully logged in.");
+    location.replace("/products/");
+  }
+}
